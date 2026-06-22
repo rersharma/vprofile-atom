933b1f84b87f648496c48defb4b953b42aae3d25
Pankaj Sharma <65856070+rersharma@users.noreply.github.com>
2026-06-22 15:26:30 +0530

    latest

--- userdata/nexus-setup.sh
+++ userdata/nexus-setup.sh
@@ -22,10 +22,8 @@ sudo chown -R nexus:nexus /opt/nexus
 #creating working directory
 sudo mkdir -p /opt/sonatype-work
 sudo chown -R nexus:nexus /opt/sonatype-work
-
 #importing servive file
 wget https://raw.githubusercontent.com/rersharma/vprofile-atom/refs/heads/master/userdata/nexus.service
-
 mv nexus.service /etc/systemd/system/nexus.service
 systemctl daemon-reload
 systemctl enable nexus
